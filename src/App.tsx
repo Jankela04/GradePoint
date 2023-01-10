76eb48446d920662222e4f9310e3a0a920b1c055
Jankela04 <jankojovanovic04@gmail.com>
2023-01-10 13:03:52 +0100

    added custom hook useLocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+]);
 
 function App() {
-    return <div className="App">Yo</div>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const useLocalStorage = (key: string, initialValue: string | number) => {
+    const [value, setValue] = useState(() => {
+        const savedItem = localStorage.getItem(key);
+
+        if (savedItem !== null) {
+            return JSON.parse(savedItem);
+        } else {
+            localStorage.setItem(key, JSON.stringify(initialValue));
+            return initialValue;
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [value]);
+
+    return {
+        value,
+        setValue,
+    };
+};
+
+export default useLocalStorage;
