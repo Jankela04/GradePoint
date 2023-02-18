eb253387353d42f7af1f8c8ad17ed547e48548ac
Jankela04 <jankojovanovic04@gmail.com>
2023-02-18 22:21:42 +0100

    added created and edited fields in db.json, created date is now displayed in NoteList

--- src/pages/Notes/pages/NewNote/components/NoteForm/NoteForm.tsx
+++ src/pages/Notes/pages/NewNote/components/NoteForm/NoteForm.tsx
@@ -2,7 +2,6 @@ import NoteCreateActions from "./components/NoteCreateActions/NoteCreateActions"
 import NoteInfo from "./components/NoteInfo/NoteInfo";
 import NoteText from "./components/NoteText/NoteText";
 import { FormEvent } from "react";
-import axios from "axios";
 import { TNote } from "../../../../components/NoteList/NoteList";
 import { v4 as uuidv4 } from "uuid";
 import { useNavigate } from "react-router-dom";
@@ -10,17 +9,31 @@ import {
     initialFormState,
     useNewNoteForm,
 } from "../../../../../../context/NewNoteFormContext";
+import formatDate from "../../../../../../utils/FormatDate";
+import axiosService from "../../../../../../services/axios";
 
 const NoteForm = () => {
     const { form, setForm } = useNewNoteForm();
 
     const navigate = useNavigate();
 
+    const createNewNote = (): TNote => {
+        return {
+            ...form,
+            id: uuidv4(),
+            created: formatDate(new Date()),
+            edited: null,
+        };
+    };
+    const saveNewNoteToServer = async (newNote: TNote) => {
+        await axiosService.post("/notes", newNote);
+    };
+
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (form.tag && form.text && form.title) {
-            const newNote: TNote = { ...form, id: uuidv4() };
-            await axios.post("http://localhost:3000/notes", newNote);
+            const newNote = createNewNote();
+            saveNewNoteToServer(newNote);
             setForm(initialFormState);
             navigate("/notes");
         } else {
