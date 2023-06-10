051f90cc3fb773bfd4ccb698a3f74ffb65c08b5c
Jankela04 <janezjovanovic04@gmail.com>
2023-06-10 12:29:43 +0200

    feat: better accessibility for NoteCard and ClassCard
    QoF change, middle clicking on Note Card or Class Card would open link in new tab

--- src/pages/Classes/ClassCard.tsx
+++ src/pages/Classes/ClassCard.tsx
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 import { v4 as uuidv4 } from "uuid";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useTheme } from "@/context/ThemeContext";
 import CalculateGpa from "@/utils/CalculateGpa";
 import { Class } from "@/types";
@@ -13,27 +13,37 @@ type Props = {
 function ClassCard({ classObj }: Props) {
     const { theme } = useTheme();
     const navigate = useNavigate();
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === "Enter") {
+            navigate(`/classes/${classObj.id}`, {});
+        }
+    };
+
     return (
-        <button
-            type="button"
+        <div
+            role="button"
+            tabIndex={0}
             className={classNames(styles.class_card, styles[theme])}
-            onClick={() => navigate(`/classes/${classObj.id}`)}
+            onKeyDown={handleKeyDown}
         >
-            <h3>{classObj.class}</h3>
-            <div className={classNames(styles.divider, styles[theme])} />
-            <div className={styles.grades}>
-                <div className={styles.grades_list}>
-                    {classObj.grades.map((grade) => (
-                        <span className={styles.grade} key={uuidv4()}>
-                            {grade.grade}
-                        </span>
-                    ))}
+            <Link to={`/classes/${classObj.id}`}>
+                <h3>{classObj.class}</h3>
+                <div className={classNames(styles.divider, styles[theme])} />
+                <div className={styles.grades}>
+                    <div className={styles.grades_list}>
+                        {classObj.grades.map((grade) => (
+                            <span className={styles.grade} key={uuidv4()}>
+                                {grade.grade}
+                            </span>
+                        ))}
+                    </div>
+                    <div className={classNames(styles.gpa, styles[theme])}>
+                        {CalculateGpa(classObj)}
+                    </div>
                 </div>
-                <div className={classNames(styles.gpa, styles[theme])}>
-                    {CalculateGpa(classObj)}
-                </div>
-            </div>
-        </button>
+            </Link>
+        </div>
     );
 }
 
